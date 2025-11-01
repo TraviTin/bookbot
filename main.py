be4ab0c85885984809765356ea166c62ed14d84f
TraviTin <scoutballoons@gmail.com>
2025-10-31 20:24:07 -0500

    added sorting

--- main.py
+++ main.py
@@ -1,15 +1,34 @@
-from stats import get_num_words
-from stats import amount_of_characters
+from stats import (
+    get_num_words, 
+    amount_of_characters,
+    chars_dict_to_sroted_list,
+)
+
 def get_book_text(file_path):
     with open(file_path) as f:
         file_contents = f.read()
         return file_contents
+
 def main():
     text = get_book_text("./books/frankenstein.txt")
+    book_path = "./books/frankenstein.txt"
     num_words = get_num_words(text)
     char_count = amount_of_characters(text)
+    chars_sorted_list = chars_dict_to_sroted_list(char_count)
+    print_report(book_path, num_words, chars_sorted_list)
+
+def print_report(book_path, num_words, chars_sorted_list):
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {book_path}...")
+    print("----------- Word Count ----------")
     print(f"Found {num_words} total words")
-    print(f"{char_count}")
+    print("--------- Character Count -------")
+    for item in chars_sorted_list:
+        if not item["char"].isalpha():
+            continue
+        print(f"{item['char']}: {item['num']}")
+    print("============= END ===============")
+    
 if __name__ == "__main__": 
     main()    
 
--- stats.py
+++ stats.py
@@ -12,5 +12,12 @@ def amount_of_characters(text):
             count[n] =1
     return count
 
+def sort(d):
+    return d["num"]
 
-
+def chars_dict_to_sroted_list(num_chars_dict):
+    sorted_list =[]
+    for ch in num_chars_dict:
+        sorted_list.append({"char": ch, "num": num_chars_dict[ch]})
+    sorted_list.sort(reverse=True, key=sort)
+    return sorted_list
